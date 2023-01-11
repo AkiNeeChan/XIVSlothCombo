24d9e720ed8d9d1fb7410b58c766bee2cd70f24f
Aki <Aki-Nee-Chan@hotmail.com>
2023-01-11 18:45:13 +0100

    6.3

--- XIVSlothCombo/Data/ActionWatching.cs
+++ XIVSlothCombo/Data/ActionWatching.cs
@@ -212,9 +212,9 @@ namespace XIVSlothCombo.Data
     internal unsafe static class ActionManagerHelper
     {
         private static readonly IntPtr actionMgrPtr;
-        internal static IntPtr FpUseAction => (IntPtr)ActionManager.fpUseAction;
-        internal static IntPtr FpUseActionLocation => (IntPtr)ActionManager.fpUseActionLocation;
-        internal static IntPtr CheckActionResources => (IntPtr)ActionManager.fpCheckActionResources;
+        internal static IntPtr FpUseAction => (IntPtr)ActionManager.MemberFunctionPointers.UseAction;
+        internal static IntPtr FpUseActionLocation => (IntPtr)ActionManager.MemberFunctionPointers.UseActionLocation;
+        internal static IntPtr CheckActionResources => (IntPtr)ActionManager.MemberFunctionPointers.CheckActionResources;
         public static ushort CurrentSeq => actionMgrPtr != IntPtr.Zero ? (ushort)Marshal.ReadInt16(actionMgrPtr + 0x110) : (ushort)0;
         public static ushort LastRecievedSeq => actionMgrPtr != IntPtr.Zero ? (ushort)Marshal.ReadInt16(actionMgrPtr + 0x112) : (ushort)0;
         public static bool IsCasting => actionMgrPtr != IntPtr.Zero && Marshal.ReadByte(actionMgrPtr + 0x28) != 0;
